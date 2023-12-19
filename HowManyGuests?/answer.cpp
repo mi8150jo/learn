3343355cd969612a68dd77b88fe91c5b7c42e913
mi8150jo <joetakutoo@gmail.com>
2023-12-19 14:40:32 +0900

    HowManyGuests?

--- HowManyGuests?/answer.cpp
+++ HowManyGuests?/answer.cpp
@@ -2,31 +2,20 @@
 using namespace std;
 
 int main(){
-    int N, Q, tmp = 0;
+    int N, Q, L[100009], R[100009], S[100009];
     cin >> N >> Q;
     int A[N];
-    int LR[2][Q];
-    int Answer[Q];
 
-
+    S[0] = 0;
     for(int i = 1; i <= N; i++){
         cin >> A[i];
-        A[i] = A[i] + A[i-1];
-        cout << A[i] << endl;
+        S[i] = S[i-1] + A[i];
+        // cout << A[i] << endl;
     }
 
-    // for(int i = 0; i < Q; i++){
-    //     cin >> LR[0][i] >> LR[1][i];
-    //     for(int x = LR[0][i]; x <= LR[1][i]; x++){
-    //         // cout << x << endl;
-    //         tmp += A[x-1];
-    //         // cout << tmp << endl;
-    //     }
-    //     Answer[i] = tmp;
-    //     tmp = 0;
-    // }
-
-    // for(int i = 0; i < Q; i++){
-    //     cout << Answer[i] << endl;
-    // }
+    for(int i = 1; i <= Q; i++){
+        cin >> L[i] >> R[i];
+        cout << S[R[i]] - S[L[i] - 1] << endl;
+    }
+    
 }
