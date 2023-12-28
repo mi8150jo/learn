d78a7830704fbc0311232cc137efee36adc65052
mi8150jo <joetakutoo@gmail.com>
2023-12-28 23:20:08 +0900

    TwiDimentionalSun

--- TwoDimensionalSum/main.cpp
+++ TwoDimensionalSum/main.cpp
@@ -2,16 +2,5 @@
 using namespace std;
 
 int main(){
-    // 入力
-    int N;
-    cin >> N;
 
-    // 2進数の変換
-    for (int x = 9; x >= 0; x--){
-        int wari = (1 << x);
-        cout << wari << endl;
-        // cout << (N / wari) % 2;
-    }
-    cout << endl;
-    return 0;
 }
