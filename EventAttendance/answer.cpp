08dd7141abd301c68d6593055396dc246a1c002c
mi8150jo <joetakutoo@gmail.com>
2023-12-29 02:16:07 +0900

    .\EventAttendance\answer.cpp

--- EventAttendance/answer.cpp
+++ EventAttendance/answer.cpp
@@ -2,29 +2,21 @@
 using namespace std;
 
 int main(){
-    int D, N, People=0;
+    int D, N;
     cin >> D >> N;
-    int L[100009], R[100009], ans[100009]={};
+    int L[100009], R[100009], ans[100009], B[100009];
     for(int i = 1; i <= N; i++){
         cin >> L[i];
         cin >> R[i];
     }
 
     for(int i = 1; i <= N; i++){
-        ans[L[i]] += 1;
-        ans[R[i]+1] -= 1;
+        B[L[i]] += 1;
+        B[R[i]+1] -= 1;
     }   
 
-    for(int i = 1; i <= D; i++){
-        People += ans[i];
-        cout << People << endl;
-    }
-
-
-    // for(int i = 1; i <= D; i++){
-    //     cout << ans[i] << " " ;
-    // }
-    // for(int i = 0; i < N; i++){
-    //     cout << L[i] << " " << R[i] << endl;
-    // }
+    // 累積和
+    ans[0] = 0;
+    for(int d = 1; d <= D; d++) ans[d] = ans[d-1] + B[d];
+    for(int d = 1; d <= D; d++) cout << ans[d] << endl;
 }
