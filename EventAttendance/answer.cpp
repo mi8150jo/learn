20bb2cecf27a97c663e3fb50eee8eccf4947df0c
mi8150jo <joetakutoo@gmail.com>
2023-12-29 01:55:30 +0900

    .\EventAttendance\

--- EventAttendance/answer.cpp
+++ EventAttendance/answer.cpp
@@ -1,30 +1,30 @@
-#include <bits/stdc++.h>
-#include <atcoder/all>
-
+#include <iostream>
 using namespace std;
-using namespace atcoder;
 
-int D,N,L,R,A_L[100100];
-
-int main()
-{
-    cin >> D;
-    cin >> N;
-
-    for (int i = 0; i < N; i++) {
-        cin >> L>>R;
-        A_L[L] ++;
-        A_L[R+1] --;
+int main(){
+    int D, N, People=0;
+    cin >> D >> N;
+    int L[100009], R[100009], ans[100009]={};
+    for(int i = 1; i <= N; i++){
+        cin >> L[i];
+        cin >> R[i];
     }
 
-    for(int i=1;i<D;i++){
-        A_L[i+1] += A_L[i];
-    }
+    for(int i = 1; i <= N; i++){
+        ans[L[i]] += 1;
+        ans[R[i]+1] -= 1;
+    }   
 
-    for (int i = 1; i < D+1; i++) {
-        cout << A_L[i] << endl;
+    for(int i = 1; i <= D; i++){
+        People += ans[i];
+        cout << People << endl;
     }
 
 
-	return 0;
+    // for(int i = 1; i <= D; i++){
+    //     cout << ans[i] << " " ;
+    // }
+    // for(int i = 0; i < N; i++){
+    //     cout << L[i] << " " << R[i] << endl;
+    // }
 }
